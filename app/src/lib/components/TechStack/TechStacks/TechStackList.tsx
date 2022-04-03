3d0fcbd20859c68a0309ba7dd3cd1110c501be18
민순기 <skmin@rsupport.com>
2022-04-03 17:25:44 +0900

    Feat create new Props 'gap' in TechStackList component #7

--- app/src/lib/components/TechStack/TechStacks/TechStackList.tsx
+++ app/src/lib/components/TechStack/TechStacks/TechStackList.tsx
@@ -6,16 +6,21 @@ interface TechStackListType {
     nameOption: { name?: string; size?: string; iconColor?: string };
     progressBarOption: { rate?: string; colorTo?: string; colorFrom?: string };
   }[];
+  gap?: 'narrower' | 'narrow' | 'normal' | 'wide' | 'wider';
 }
 
-const TechStackList = ({ techStackList }: TechStackListType) => {
+const TechStackList = ({ techStackList, gap = 'normal' }: TechStackListType) => {
   return (
     <div id="TechStacks">
       {techStackList?.map(({ nameOption, progressBarOption }, idx) => (
-        <TechStack key={idx} nameOption={nameOption} progressBarOption={progressBarOption} />
+        <TechStack key={idx} nameOption={nameOption} progressBarOption={progressBarOption} gap={gap} />
       ))}
     </div>
   );
 };
 
 export default TechStackList;
+
+TechStackList.defaultProps = {
+  gap: 'normal',
+};
