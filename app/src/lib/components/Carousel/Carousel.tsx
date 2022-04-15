5a3b0903aa6c3085db60b6c4cfde6fce2b2a0dcd
민순기 <skmin@rsupport.com>
2022-04-15 16:46:40 +0900

    [Update] change ui #7

--- app/src/lib/components/Carousel/Carousel.tsx
+++ app/src/lib/components/Carousel/Carousel.tsx
@@ -27,12 +27,14 @@ const Carousel = ({ children, transistion = 1000, autoplaySpeed = 3000 }: Props)
   useInterval(showNext, autoplaySpeed, [showIndex]);
 
   return (
-    <Wrapper len={childrenLen} transistion={transistion}>
+    <Wrapper len={childrenLen} transistion={transistion} showIndex={showIndex}>
       <FaArrowCircleLeft id="prev-button" onClick={showPrev} />
-      <div className="carousel-container" style={{ transform: `translate3d(${-showIndex * 100}vw, 0, 0)` }}>
-        {React.Children.map(children, (child) => {
-          return <Container key={child?.toString()}>{child}</Container>;
-        })}
+      <div className="carousel-wrapper">
+        <div className="carousel-container">
+          {React.Children.map(children, (child) => {
+            return <Container key={child?.toString()}>{child}</Container>;
+          })}
+        </div>
       </div>
       <FaArrowCircleRight id="next-button" onClick={showNext} />
     </Wrapper>
@@ -49,22 +51,35 @@ Carousel.defaultProps = {
 const Wrapper = styled.div<{
   len: number;
   transistion: number;
+  showIndex: number;
 }>`
   overflow: hidden;
+  position: relative;
   #prev-button,
   #next-button {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
     z-index: 3;
     cursor: pointer;
   }
-
+  #next-button {
+    right: 0;
+  }
+  .carousel-wrapper {
+    width: 100%;
+  }
   .carousel-container {
+    display: flex;
     position: relative;
     transition: ${(props) => props.transistion / 1000}s;
-    width: ${(props) => `calc(${props.len} * 100vw)`};
+    width: ${(props) => `calc(${props.len} * 100%)`};
+    transform: ${(props) => `translateX(${(-props.showIndex * 100) / props.len}%)`};
   }
 `;
 
 const Container = styled.div`
-  float: left;
-  width: 100vw;
+  display: flex;
+  justify-content: center;
+  width: 100%;
 `;
