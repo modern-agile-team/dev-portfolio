9c1973b50b9666bbeba7629f010be78c7ea30b48
seohyunsim <ssh4563@naver.com>
2022-04-20 00:47:49 +0900

    [Update] modify masonry and add des in image components (#7)

--- app/src/lib/components/Masonry/Image.tsx
+++ app/src/lib/components/Masonry/Image.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 interface Props {
   src: string;
+  title?: string;
+  description?: string;
 }
 
-const Image = ({ src }: Props) => {
+const Image = ({ src, title, description }: Props) => {
   return (
     <Wrap>
       <Items>
         <img src={src} />
+        <div className="imgDes">
+          <p>{title}</p>
+          <span>{description}</span>
+        </div>
       </Items>
     </Wrap>
   );
@@ -17,21 +23,40 @@ const Image = ({ src }: Props) => {
 
 export default Image;
 
+Image.defaultProps = {
+  title: '제목을 입력하세요',
+  description: '본문을 입력하세요',
+};
+
 const Wrap = styled.div`
   display: flex;
   justify-content: center;
-  margin-bottom: 1em;
+  margin-bottom: 1.5em;
+  display: inline;
 `;
 
 const Items = styled.div`
   display: inline-block;
   width: 100%;
+  overflow: hidden;
+  margin-bottom: 0.5em;
   img {
     width: 100%;
     cursor: pointer;
-    transition: 0.3s;
+    transition: 0.6s;
+    object-fit: cover;
     :hover {
       transform: scale(105%);
+    }
+  }
+  .imgDes {
+    p {
+      margin-left: 1px;
+      font-size: 14px;
+    }
+    span {
+      font-size: 22px;
+      font-weight: 800;
     }
   }
 `;
