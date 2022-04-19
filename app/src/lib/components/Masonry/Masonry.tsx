38cfbfcb17c7872e83b30ec7e904a14eaaf906b1
seohyunsim <ssh4563@naver.com>
2022-04-20 00:51:01 +0900

    [Remove] delete unused code in masonry components (#7)

--- app/src/lib/components/Masonry/Masonry.tsx
+++ app/src/lib/components/Masonry/Masonry.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 interface Props {
   children: React.ReactNode;
   padding?: string;
   column?: number;
-  theme?: 'mid-night' | 'blossom' | 'fruits' | 'bare-bare' | 'tropic-green' | 'poppy';
 }
 
-const Masonry = ({ children, padding, column, theme }: Props) => {
+const Masonry = ({ children, padding, column }: Props) => {
   return (
-    <Wrap theme={theme} padding={padding} column={column}>
+    <Wrap padding={padding} column={column}>
       {children}
     </Wrap>
   );
@@ -18,54 +17,11 @@ const Masonry = ({ children, padding, column, theme }: Props) => {
 
 export default Masonry;
 
-Masonry.defaultProps = {
-  theme: 'poppy',
-};
-
 const Wrap = styled.div<{
   padding?: string;
   column?: number;
-  theme?: 'mid-night' | 'blossom' | 'fruits' | 'bare-bare' | 'tropic-green' | 'poppy';
 }>`
   padding: ${({ padding }) => padding ?? '4em'};
   column-count: ${({ column }) => column ?? 4};
   column-gap: 1.5em;
-  .masonry-item {
-    .hover {
-      ${({ theme }) => {
-        switch (theme) {
-          case 'mid-night':
-            return css`
-              background-color: #0c1631b9;
-              color: #fff;
-            `;
-          case 'blossom':
-            return css`
-              background-color: #ffa1b2b8;
-              color: #fff;
-            `;
-          case 'fruits':
-            return css`
-              background-color: #fbbd5ac5;
-              color: #fa4529;
-            `;
-          case 'bare-bare':
-            return css`
-              background: linear-gradient(#c2e3f4b1, 70%, #efb630b1);
-              color: #030305;
-            `;
-          case 'tropic-green':
-            return css`
-              background-color: #007f53a9;
-              color: #f6f1ed;
-            `;
-          case 'poppy':
-            return css`
-              background-color: #90c8b6c0;
-              color: #ff4848;
-            `;
-        }
-      }}
-    }
-  }
 `;
