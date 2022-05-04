4bc6f29839e0285d54fa3c097dc4fec8c044f507
seohyunsim <ssh4563@naver.com>
2022-05-04 23:28:10 +0900

    [Chore] update style code from feedback (#7)

--- app/src/lib/components/Experience/History.tsx
+++ app/src/lib/components/Experience/History.tsx
@@ -35,24 +35,23 @@ const History = (props: Props) => {
 export default History;
 
 const Wrap = styled.div`
-  width: 80%;
+  width: 85%;
   margin: 0 auto;
   display: flex;
   justify-content: space-between;
   padding: 2.2em 2em 3.2em 2em;
   border-bottom: 0.2px solid #b4b4b4a2;
   .intro {
-    min-width: 40%;
+    min-width: 42%;
     display: flex;
     justify-content: space-between;
     .date {
       display: flex;
-      flex-direction: column;
       justify-content: space-between;
       .start-date {
         ::after {
           content: '-';
-          margin: 0px 10px;
+          margin: 0px 7px;
         }
       }
       .end-date {
@@ -60,13 +59,13 @@ const Wrap = styled.div`
       }
     }
     .child-title {
-      max-width: 60%;
+      max-width: 58%;
       font-size: 22px;
       font-weight: 600;
     }
   }
   .des {
-    max-width: 45%;
+    max-width: 50%;
     text-align: center;
   }
 `;
