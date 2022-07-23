786206b4cf5fb1250d71e946cec9420ee06c7e3c
woorim960 <woorimprog@gmail.com>
2022-07-23 10:59:17 +0900

    [Refactor] Change wrong names 'uri' to 'url' #45

--- app/src/lib/components/Header/Logo.tsx
+++ app/src/lib/components/Header/Logo.tsx
@@ -34,10 +34,10 @@ const Title = styled.span<{
 `;
 
 const Logo = ({ logoOption }: LogoProps): React.ReactElement => {
-  const { uriToMove, logoImg, title, styles } = logoOption;
+  const { urlToMove, logoImg, title, styles } = logoOption;
   const { logoMargin, titleColor, titleSize, titleWeight } = styles;
   return (
-    <Container href={uriToMove}>
+    <Container href={urlToMove}>
       <LogoImgContainer logoMargin={logoMargin}>
         {logoImg ? <img src={logoImg} /> : <Icon icon="simple-icons:devdotto" color="#434521" fontSize="80px" />}
       </LogoImgContainer>
