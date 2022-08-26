4f4b17fbc1cdb9ae137c2edc9911950df5ad5f0a
seohyunsim <ssh4563@naver.com>
2022-08-26 17:28:13 +0900

    [Chore] add justify-content in vertical theme (#90)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -19,7 +19,7 @@ import {
 function App() {
   return (
     <div className="App">
-      <VisitorComment />
+      <VisitorComment theme="vertical" />
       <CardWrap id="카드에연">
         <Card width="20vw" height="20vw" hover="down" redirectURL="/1">
           This is Card component
--- app/src/lib/components/VisitorComment/CommentList/Vertical.tsx
+++ app/src/lib/components/VisitorComment/CommentList/Vertical.tsx
@@ -30,6 +30,7 @@ const Wrap = styled.div`
   box-shadow: 0px 4px 10px 1px rgba(0, 0, 0, 0.04);
   background-color: white;
   text-align: center;
+  justify-content: center;
   .des {
     padding: 1em;
     border-bottom: 1px solid #b4b4b4a2;
