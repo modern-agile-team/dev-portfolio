8b3f9be48072e26c3873c021e1bcc87f5eeb8b48
Min <alstnsrl98@gmail.com>
2022-09-09 22:44:53 +0900

    [Docs]: add Intro Component type docs (#111)

--- app/src/lib/common/types/ComponentTypes/IntroType.ts
+++ app/src/lib/common/types/ComponentTypes/IntroType.ts
@@ -1,9 +1,18 @@
 export interface IntroTitlePropsType {
+  /**
+   * Core of your introduction
+   */
   title?: string;
+  /**
+   * a brief introduction
+   */
   shortIntro?: string;
 }
 
 export interface IntroFormPropsType {
+  /**
+   * Introduce yourself
+   */
   description?: string;
 }
 
@@ -12,8 +21,20 @@ type IntroTextAlignType = 'left' | 'center';
 export interface IntroOptionType extends IntroTitlePropsType, IntroFormPropsType {}
 
 export interface IntroPropsType {
+  /**
+   * Name to be added to Sidebar
+   */
   id?: string;
+  /**
+   * Intro Text align	center (default: center)
+   */
   textAlign?: IntroTextAlignType;
+  /**
+   * Intro Background color	whitesmoke (default: whitesmoke)
+   */
   backgroundColor?: string;
+  /**
+   * Intro-only props such as title, description, etc	(default: {@link https://github.com/modern-agile-team/dev-portfolio/blob/master/app/src/lib/common/types/ComponentTypes/IntroType.ts "See More about Intro's Props"})
+   */
   introOption?: IntroOptionType;
 }
