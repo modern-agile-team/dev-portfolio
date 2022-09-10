72f5e669bb5169b03e7711ea5f375e8c5f83af28
seohyunsim <ssh4563@naver.com>
2022-09-10 23:14:12 +0900

    [Update] refine the default props of components

--- app/src/lib/common/types/ComponentTypes/Header/HeaderType.ts
+++ app/src/lib/common/types/ComponentTypes/Header/HeaderType.ts
@@ -4,9 +4,9 @@ import { HeaderLogoPropsType } from './HeaderLogoType';
 
 export interface HeaderPropsType extends HeaderStyledPropsType {
   id?: string;
-  logoOption: HeaderLogoPropsType;
+  logoOption?: HeaderLogoPropsType;
   channels?: ChannelType[];
-  sideBarOption: SideBarOptionPropsType;
+  sideBarOption?: SideBarOptionPropsType;
 }
 
 export interface HeaderStyledPropsType {
