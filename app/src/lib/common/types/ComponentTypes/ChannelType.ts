c56de2124c0ce1f79350c07290dd1bbd4971bdc6
soonki-98 <alstnsrl98@gmail.com>
2022-09-10 15:22:15 +0900

    [Docs] add typedoc of Channel Component (#111)

--- app/src/lib/common/types/ComponentTypes/ChannelType.ts
+++ app/src/lib/common/types/ComponentTypes/ChannelType.ts
@@ -3,12 +3,27 @@ export interface ChannelPropsType {
 }
 
 export interface ChannelType extends ChannelStyledType {
+  /**
+   * link to channel
+   */
   redirectUrl: string;
+  /**
+   * Channel name
+   */
   name: string;
+  /**
+   * Color of Channel Icon
+   */
   color?: string;
+  /**
+   * Size of Channel Icon (unit: px)
+   */
   size?: string;
 }
 
 export interface ChannelStyledType {
+  /**
+   * Css margin of each Icons
+   */
   margin?: string;
 }
