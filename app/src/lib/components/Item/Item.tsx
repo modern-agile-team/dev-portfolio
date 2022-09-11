97d3820177f59eca1e2b1d70001dfeecf004c121
seohyunsim <ssh4563@naver.com>
2022-09-12 00:00:09 +0900

    [Chore] change default image in item component (#120)

--- app/src/lib/components/Item/Item.tsx
+++ app/src/lib/components/Item/Item.tsx
@@ -79,8 +79,7 @@ Item.defaultProps = {
   redirectURL: '',
   title: 'This is title',
   description: 'description',
-  imgURL:
-    'https://catnip-echium-964.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fba8bdb9c-c600-453f-bdb7-9c04419b026c%2Fdefault.png?table=block&id=440c81c7-fdfa-4688-87bd-51215d4ef7d3&spaceId=0b241d7f-6520-4240-ac94-27957e3f3aa5&width=2000&userId=&cache=v2',
+  imgURL: 'https://picsum.photos/500/300/?random',
   textRisingSpeed: 300,
 };
 
