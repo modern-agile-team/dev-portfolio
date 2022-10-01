76febcc55b492ad0f336a1f47d81901f94d0dc1c
woorim960 <woorimprog@gmail.com>
2022-10-01 12:29:26 +0900

    [Chore] add typedoc comment in SkillType.ts #153

--- src/lib/common/types/ComponentTypes/SkillType.ts
+++ src/lib/common/types/ComponentTypes/SkillType.ts
@@ -1,10 +1,34 @@
 export interface SkillPropsType {
+  /**
+   * Name to be added to Sidebar (default: 'Skill')
+   */
   id?: string;
+  /**
+   * Main text that expresses the skill (default: 'dev-portfolio')
+   */
   title?: string;
+  /**
+   * Title size style (default: '24px')
+   */
   titleSize?: string;
+  /**
+   * If this value is set to True, you can hide the title. (default: false)
+   */
   isHiddenTitle?: string;
+  /**
+   * Enter the name of the icon you searched on the following site. (default: 'simple-icons:devdotto') {@link https://icon-sets.iconify.design/}
+   */
   iconName?: string;
+  /**
+   * Icon size style (defualt: '50px')
+   */
   iconSize?: string;
+  /**
+   * Skill margin style (defualt: '0px')
+   */
   margin?: string;
+  /**
+   * Skill padding style (defualt: '0px')
+   */
   padding?: string;
 }
